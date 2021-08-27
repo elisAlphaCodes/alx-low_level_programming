f64038918a4c2bea3b78c628a058e9388eccc37a
elisAlphaCodes <eli.joshua55@gmail.com>
2021-08-27 11:22:46 -0400

    Patience, persistence and perspiration make an unbeatable combination for succes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar(48 + number);
+		if (number != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
