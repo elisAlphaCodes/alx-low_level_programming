cb6ffc9cffa1d8517db5d5bbd5caa7be25819227
elisAlphaCodes <eli.joshua55@gmail.com>
2021-08-31 04:29:36 -0400

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c[10] = "Holberton";
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
