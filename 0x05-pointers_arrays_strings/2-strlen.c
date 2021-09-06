891ac82673c50e478e445fddc0d435e24ce98862
elisAlphaCodes <eli.joshua55@gmail.com>
2021-09-06 07:11:32 -0400

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[1])
+	{
+		i++;
+	}
+	return (i);
+}
