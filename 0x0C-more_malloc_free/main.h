292426cdc36f5bb456a2f864efb55df92a43d67c
elisAlphaCodes <70447623+elisAlphaCodes@users.noreply.github.com>
2021-09-24 17:01:01 -0400

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,7 +8,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 
 #endif
